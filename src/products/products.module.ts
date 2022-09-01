9c5d0916148bd079d7ea76a4cc6c94a89dcd7eaf
nomanluhar <luharnoman409@gmail.com>
2022-09-01 23:56:06 +0530

    First push

---
+++ src/products/products.module.ts
@@ -0,0 +1,14 @@
+/*
+https://docs.nestjs.com/modules
+*/
+
+import { Module } from '@nestjs/common';
+import { ProductsController } from './products.controller';
+import { ProductsService } from './products.service';
+
+@Module({
+    imports: [],
+    controllers: [ProductsController],
+    providers: [ProductsService],
+})
+export class ProductsModule { }
